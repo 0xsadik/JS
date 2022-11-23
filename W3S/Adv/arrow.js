9302833dbcf2c9ddd097f67921edd1c587ffdc93
0xsadik <torikussadik404@gmail.com>
2022-11-23 08:31:06 +0600

    arrow

---
+++ W3S/Adv/arrow.js
@@ -0,0 +1,41 @@
+/**
+ *      javascript Arrow Function
+ * 
+ * Arrow function allow us oto write shorter function syntax:
+ * 
+ */
+
+
+//      before arrow 
+
+// let hello = function() {
+//     return 'hello world';
+// }
+// console.log(hello());
+
+
+
+//      with arrow function
+
+// let hello = () => {
+//     return 'hello wrold';
+// }
+// console.log(hello());
+
+
+
+//      arrow function return value by default :
+
+
+// let hello = () => 'hello world!';
+// console.log(hello());
+
+
+//      arrow function with parameters:
+
+
+// hello = (val) => 'hello' + val;
+// console.log(hello(' world'));
+
+
+
