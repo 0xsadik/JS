c8857478ad67a359575598c01dc1ca729776735b
0xsadik <torikussadik404@gmail.com>
2022-11-23 08:43:51 +0600

    class

---
+++ W3S/Adv/class.js
@@ -0,0 +1,51 @@
+//              javascript classes 
+
+// syntax 
+
+
+// class ClassName {
+//     constructor () {
+//         .... 
+//     }
+// }
+
+
+// class Car {
+//     constructor(name, year) {
+//         this.name = name;
+//         this.year = year;
+//     }
+// }
+
+
+//      using a class
+
+// class Car {
+//     constructor(name, year) {
+//         this.name = name;
+//         this.year = year;
+//     }
+// }
+// let myCar1 = new Car('Ford', 2014);
+// let myCar2 = new Car('Audi', 2019);
+// console.log(myCar2.name, myCar2.year);
+
+
+
+
+// class Car {
+//     constructor(name, year) {
+//         this.name = name;
+//         this.year = year;
+//     }
+//     owner() {
+//         let name = 'Torikus';
+//         return name;
+//     }
+// }
+
+// let car = new Car('Ferari', 2022);
+// let carowner = car.owner();
+// console.log(car.name, carowner);
+
+
