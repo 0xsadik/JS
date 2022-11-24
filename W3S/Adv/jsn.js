a0d6e66b45f0bc2a441804ce7333c6540b66d520
0xsadik <torikussadik404@gmail.com>
2022-11-24 09:11:18 +0600

    json

---
+++ W3S/Adv/jsn.js
@@ -0,0 +1,19 @@
+/**
+ *      javascript json
+ * 
+ * JSON is a fromat for storing and transporting data
+ * JSON is often used when data is sent from a server to a web page
+ * 
+ * JSON stands for JavaScript Object Notation
+ */
+
+//      example 
+
+{
+    "employees": [
+        {"firstName": "Shakil", "lastName": "sadik"},
+        {"firstName": "Fahim", "lastName": "morshed"},
+        {"firstName": "Torikus", "lastName": "sadik"}
+    ]
+}
+
