cd9687ccf7597002b5a38dddf4651e21450a3d55
0xsadik <torikussadik404@gmail.com>
2022-11-25 21:32:09 +0600

    for of

---
+++ W3S/ES6/for_of.js
@@ -0,0 +1,20 @@
+//      The for / of loop 
+
+//  looping over an array
+
+
+const frnds = ['shakil','babu','fahim','morshed','abdul halim','ahosan ullah', 'arafat hossain'];
+
+for (let x of frnds) {
+    console.log(x);
+}
+
+
+
+//      looping over an string
+
+
+let language = "javaScript";
+for (let s of language) {
+    console.log(s);
+}
