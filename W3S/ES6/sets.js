83ff9f8db7ae85b143267342a93090a35e179b61
0xsadik <torikussadik404@gmail.com>
2022-11-25 21:39:53 +0600

    sets

---
+++ W3S/ES6/sets.js
@@ -0,0 +1,12 @@
+//      javascript Sets 
+
+
+const letters = new Set();
+letters.add("t");
+letters.add("o");
+letters.add("r");
+letters.add("i");
+letters.add("k");
+letters.add("u");
+letters.add("s");
+console.log(letters);
