4e4677ad66db11b0e1cbc6fb52af3605f56ec0a2
0xsadik <torikussadik404@gmail.com>
2022-11-25 22:09:16 +0600

    math methods

---
+++ W3S/ES6/math_methods.js
@@ -0,0 +1,29 @@
+//      new math methods 
+
+
+//  the Math.turnc() 
+
+console.log(Math.trunc(4.9));
+console.log(Math.trunc(4.6));
+console.log(Math.trunc(4.4));
+console.log(Math.trunc(4.3));
+console.log(Math.trunc(-4.3));
+
+//  the Math.sign()
+
+console.log(Math.sign(-4));
+console.log(Math.sign(0));
+console.log(Math.sign(4));
+
+
+//  the Math.cbrt()
+
+console.log(Math.cbrt(8));
+console.log(Math.cbrt(64));
+console.log(Math.cbrt(125));
+
+
+//  the Math.log2()
+
+console.log(Math.log2(2));
+console.log(Math.log2(10));
