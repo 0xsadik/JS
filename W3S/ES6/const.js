e2a15624ca0bc7971095542f3c1dac63e117d793
0xsadik <torikussadik404@gmail.com>
2022-11-25 21:17:37 +0600

    const

---
+++ W3S/ES6/const.js
@@ -0,0 +1,15 @@
+/**
+ * The const keyword allows you to declare a constan ( a javascript variable with a constant value)
+ * constants are similar to let variable, except that the value cannot be changed
+ */
+
+
+
+var x = 10;
+// here x is 10
+{
+    const x = 20;
+    // here x is 20
+}
+// here x is 10
+
