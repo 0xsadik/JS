6fbeb6b2a785847590c2e5bff04bdd6f892cd157
0xsadik <torikussadik404@gmail.com>
2022-11-25 21:51:59 +0600

    string.includes

---
+++ W3S/ES6/includes.js
@@ -0,0 +1,9 @@
+//      String.includes()
+
+//      The includes() methods returns true if a string contains a specified value, otherwise false;
+
+
+let text = "I love ya very much";
+let found = text.includes("ya");
+console.log(found);
+
