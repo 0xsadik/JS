f6d5a10cc89d12b0b5f63c7f03abf2bc4329c217
0xsadik <torikussadik404@gmail.com>
2022-11-27 21:36:26 +0600

    length

---
+++ Array/length.js
@@ -0,0 +1,8 @@
+//  length property and direct assignment 
+
+const pets = ['dog', 'cat', 'rabbit', 'hamster'];
+pets[0] = "horse";
+const petsLength = pets.length;
+
+console.log(petsLength);
+console.log(pets)
