357b216343cf1e6c8afc7d9cdd69f004025ae8d2
0xsadik <torikussadik404@gmail.com>
2022-11-27 13:52:50 +0600

    fundamentals

---
+++ PH/Intro/conversion.js
@@ -0,0 +1,38 @@
+// var onionPrice = 42;
+// var eggPrice = '41';
+// var totalPrice = onionPrice + eggPrice;
+// console.log(totalPrice);
+
+
+
+// var onionPrice = '42';
+// var eggPrice = 42;
+// var totalPrice = onionPrice + eggPrice;
+// console.log(totalPrice);
+
+
+// integer 
+
+var sunglass = 3;
+
+// float 
+
+var price = 99.99;
+
+
+// to integer 
+
+// var onionPrice = '42';
+var onionPrice = '42.434';
+var onionPriceNumber = parseInt(onionPrice);
+// console.log(onionPriceNumber);
+
+
+// to float 
+
+// var eggPrice = '12';
+var eggPrice = '13.23';
+var eggPriceNumber = parseFloat(eggPrice);
+// console.log(eggPriceNumber);
+
+
---
+++ PH/Intro/maths.js
@@ -0,0 +1,5 @@
+var first = 0.1;
+var second = 0.2;
+var total = first + second;
+// console.log(total);
+console.log(parseFloat(total.toFixed(3)));
