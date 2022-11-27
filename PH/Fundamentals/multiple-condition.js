2446f0f25d0e401530b39f2dbfb84ab30bc0e174
0xsadik <torikussadik404@gmail.com>
2022-11-27 15:54:37 +0600

    +

---
+++ PH/Fundamentals/multiple-condition.js
@@ -0,0 +1,28 @@
+// var gotJob = true;
+// var gotJob = true;
+// var moneySaved = 250000;
+
+// if ( gotJob == true && moneySaved > 200000) {
+//     console.log('cholo biye kore feli');
+// }
+// else {
+//     console.log('tor kopale biya nai');
+// }
+
+
+
+
+var danishPrice = 50;
+var butterBreakPrice = 25;
+var toastBiscuitPrice = 10;
+var myBudget = 100;
+
+if (danishPrice < myBudget) {
+    console.log('Danish kheye danish jabo');
+}
+else if (danishPrice < myBudget) {
+    console.log('Butter bon diye cha khamu');
+}
+else if (toastBiscuitPrice < myBudget){
+    console.log('toast biscuit khamu');
+}
