03efacf02eebae8de347c4a905134c2147ac8764
0xsadik <torikussadik404@gmail.com>
2022-11-27 13:55:52 +0600

    variables

---
+++ PH/Fundamentals/variable.js
@@ -0,0 +1,17 @@
+//    Number 
+
+var watchPrice = 120;
+watchPrice = 125;
+
+// console.log(watchPrice);
+
+
+//  string 
+
+var userName = "Sogir uddin";
+
+
+//  boolean
+
+var isEspensive = true;
+
