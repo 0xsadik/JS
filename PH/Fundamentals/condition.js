3458b96268d703bf1bd73894298ecc972bad2a00
0xsadik <torikussadik404@gmail.com>
2022-11-27 15:28:24 +0600

    +

--- PH/Fundamentals/condition.js
+++ PH/Fundamentals/condition.js
@@ -1,6 +1,29 @@
 
-let isFoodReady = true;
+// let isFoodReady = true;
 
-if(isFoodReady == true) {
-    console.log('Mama! i wil eat now.');
+// if(isFoodReady == true) {
+//     console.log('Mama! i wil eat now.');
+// }
+
+
+var iphonePrice = 70000;
+var myBudget = 9500;
+// if (iphonePrice < myBudget) {
+//     console.log('Iphone dia futani marbo.!');
+// }
+
+
+// if (iphonePrice > myBudget) {
+//     console.log('my mi phone is best!');
+// }
+
+
+var chickenPrice = 180;
+var myMoney = 50;
+
+if (chickenPrice < myMoney) {
+    console.log('yes! murgir ran khamu ar haddi cabamu');
+}
+else {
+    console.log('smashed potato with lentil soup'); 
 }
