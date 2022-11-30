dc5410a8d4fef4c96c2ada0c1928874134f7afa7
0xsadik <torikussadik404@gmail.com>
2022-11-30 21:26:24 +0600

    challange

---
+++ Array/Advanced Methods/challange.js
@@ -0,0 +1,48 @@
+const movies = [
+    {
+        title: 'Avater',
+        year: '2009',
+        rating: 'PG-13'
+    },
+    {
+        title: 'I am Legend',
+        year: '2007',
+        rating: 'PG-13'
+    },
+    {
+        title: '300', 
+        year: '2006',
+        rating: 'R'
+    },
+    {
+        title: 'The avengers',
+        year: '2012',
+        rating: 'PG-13'
+    },
+    {
+        title: 'The wolf of wall street',
+        year: '2013',
+        rating: 'R'
+    }
+];
+
+// Challange question #1
+// Create a new array that just contains the movie titles.
+
+// let movieTitle = movies.map((movie) => {
+//     return movie.title; 
+// });
+// console.log(movieTitle);
+
+// Challange question #2
+// Create a new array that contains only those movies that are PG-13
+// let specific = movies.filter((movie) => {
+//     return movie.rating === 'PG-13';
+// });
+// console.log(specific);
+
+// Challange question #3
+// Determin how many of the movies are rating R.
+
+
+
