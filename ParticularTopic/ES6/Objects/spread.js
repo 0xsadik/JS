99019581a577958fe642ae66bfcec14fdbb779ac
0xsadik <torikussadik404@gmail.com>
2022-12-01 14:49:31 +0600

    spread

---
+++ ParticularTopic/ES6/Objects/spread.js
@@ -0,0 +1,19 @@
+
+const daytime = {
+    breakfast: 'oatmeal',
+    lunch : 'peanut butter and jelly'
+};
+
+const nighttime = 'mac and cheese';
+
+// const backpackingMeals = {
+//     daytime,
+//     nighttime
+// }
+
+
+const backpackingMeals = {
+    ...daytime,
+    nighttime
+}
+console.log(backpackingMeals);
