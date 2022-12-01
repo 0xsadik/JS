37e553d9b98c609908cbaee8add84adad8196fb4
0xsadik <torikussadik404@gmail.com>
2022-12-01 16:24:23 +0600

    inheritance

---
+++ ParticularTopic/ES6/Objects/inherit.js
@@ -0,0 +1,21 @@
+// inheritance with javaScript classes
+
+class Vehicle {
+    constructor (description, wheels) {
+        this.description = description;
+        this.wheels = wheels;
+    }
+
+    describeYourself() {
+        console.log(`I am a ${this.description} with ${this.wheels} wheels.`);
+    }
+};
+
+class SemiTruck extends Vehicle {
+    constructor() {
+        super("semi truck", 18);
+    }
+}
+
+let groceryStoreSemi = new SemiTruck();
+groceryStoreSemi.describeYourself();
