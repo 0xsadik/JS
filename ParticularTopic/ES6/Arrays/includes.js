13b6a90a3b945286f9ce51a421dfda190f2caf81
0xsadik <torikussadik404@gmail.com>
2022-12-01 14:31:51 +0600

    indludes()

---
+++ ParticularTopic/ES6/Arrays/includes.js
@@ -0,0 +1,11 @@
+
+let cities = [
+    'spokane',
+    'boston',
+    'los angeles',
+    'seattle',
+    'portland'
+];
+
+console.log(cities.includes('boston'));
+console.log(cities.includes('barbera'));
