fec02c551623f8e16de719ad3eee42e6e5f88e80
0xsadik <torikussadik404@gmail.com>
2022-12-01 14:29:50 +0600

    destructuring

---
+++ ParticularTopic/ES6/Arrays/destructuring.js
@@ -0,0 +1,20 @@
+let cities = [
+    'spokane',
+    'boston',
+    'los angeles',
+    'seattle',
+    'portland'
+];
+
+
+let [first, , third] = [
+    'spokane',
+    'boston',
+    'los angeles',
+    'seattle',
+    'portland'
+];
+
+// console.log(cities[0]);
+console.log(first);
+console.log(third);
