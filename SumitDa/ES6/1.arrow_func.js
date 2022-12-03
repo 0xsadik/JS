93861251a5ebb19583c80cb1ac4d61140b75b5fb
0xsadik <torikussadik404@gmail.com>
2022-12-03 09:09:57 +0600

    arrow

---
+++ SumitDa/ES6/1.arrow_func.js
@@ -0,0 +1,35 @@
+// ES6 fat arrow function 
+
+
+// function number() {
+//     return 10;
+// }
+
+
+// arrow
+
+// let number = () => {
+//     return 10; 
+// }
+
+
+// let number = () => 10;
+
+
+// function number(n) {
+//     return n;
+// }
+
+
+// let number = (n) => {
+//     return n;
+// }
+
+
+// let number = (n) => 15;
+
+// let number = n => 15;
+
+let number = (a, b) => a + b;
+
+console.log(number(12, 56));
