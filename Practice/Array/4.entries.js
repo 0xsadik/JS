3bc9c26a4beb6c1eccad1930134e5255af3fa18e
0xsadik <torikussadik404@gmail.com>
2022-12-04 21:30:53 +0600

    entries

---
+++ Practice/Array/4.entries.js
@@ -0,0 +1,34 @@
+
+// const arr = ['a', 'b', 'c'];
+// const iterator = arr.entries();
+// console.log(iterator.next().value);
+// console.log(iterator.next().value);
+
+
+
+
+// iterating with index  and element 
+
+// const arr = ['a', 'b', 'c'];
+
+// for (const [index, element] of arr.entries()) {
+//     console.log(index, element);
+// }
+
+
+// using for..of loop
+
+// const arr = ['a', 'b', 'c'];
+// const arrEntries = arr.entries();
+
+// for (elements of arrEntries) {
+//     console.log(elements);
+// }
+
+
+//  iterating sparse arrays 
+
+// for (const element of [, 'a'].entries()) {
+//     console.log(element);
+// }
+
