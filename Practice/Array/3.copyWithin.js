4a5d0dfce8fecd71745bc7672da595359dd9fac3
0xsadik <torikussadik404@gmail.com>
2022-12-04 18:51:54 +0600

    copyWithin

---
+++ Practice/Array/3.copyWithin.js
@@ -0,0 +1,24 @@
+// copyWithin(target)
+// copyWithin(target, start)
+// copyWithin(target, start, end)
+
+
+const arr = ['a', 'b', 'c', 'd', 'e', 'f'];
+
+// console.log(arr.copyWithin(0, 4));
+
+// console.log(arr.copyWithin(0, 3, 4));
+// console.log(arr.copyWithin(0, 3, 5));
+
+
+// console.log([1, 2, 3, 4, 5].copyWithin(-2));
+
+// console.log([1, 2, 3, 4, 5].copyWithin(0, 3));
+
+// console.log([1, 2, 3, 4, 5].copyWithin(0, 3, 4));
+
+// console.log([1, 2, 3, 4, 5].copyWithin(-2, -3, -1));
+
+
+
+
