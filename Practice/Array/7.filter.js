41e360de367f8c9731a14cbfb015360fb29b438e
0xsadik <torikussadik404@gmail.com>
2022-12-05 15:01:08 +0600

    filter

---
+++ Practice/Array/7.filter.js
@@ -0,0 +1,37 @@
+// Arrow function
+// filter((element) => { /* … */ })
+// filter((element, index) => { /* … */ })
+// filter((element, index, array) => { /* … */ })
+
+// const words = ['spray', 'elite', 'limit', 'exuberant', 'destruction', 'present'];
+// const result = words.filter((word) => word.length > 6);
+// console.log(result);
+
+
+// filtering out all small value 
+
+// function isBigEnough(value) {
+//     return value >= 10;
+// }
+
+// const filtered = [12, 5, 8, 6, 4, 9, 10, 130, 44].filter(isBigEnough);
+// console.log(filtered);
+
+
+
+//  find all prime numbers in an array
+
+// const arr = [-3, -2, -1, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13];
+
+// function isPrime(num) {
+//     for (let i = 2; num > i; i++) {
+//         if (num % 2 === 0) {
+//             return false;
+//         }
+//     }
+//     return num > 1;
+// }
+
+// console.log(arr.filter(isPrime));
+
+
