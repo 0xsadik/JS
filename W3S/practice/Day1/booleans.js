a469345deaa871f0ab4feb8a34a594ad9a9f39ba
0xsadik <torikussadik404@gmail.com>
2022-12-08 23:05:35 +0600

    practice

---
+++ W3S/practice/Day1/booleans.js
@@ -0,0 +1,133 @@
+
+// == boolean values ==
+
+// let isLightOn = true;
+// let isRaining = false;
+// let truValue = 4 > 3;
+// let falseValue = 4 < 3; 
+
+// -- undefined -- 
+
+// let firstName
+// console.log(firstName);
+
+
+// -- null -- 
+
+let empty  = null;
+// console.log(empty);
+
+
+// -- operators -- 
+
+// let firstName = 'Torikus';
+// let country = 'Bangladesh';
+
+
+
+// -- arthmetic operators -- 
+
+// a + b
+// a - b 
+// a * b
+// a / b 
+// a % b 
+// a ** b 
+// +  -  * / % ** 
+
+
+// -- comparison operators -- 
+
+// == ( equal in value only )
+// === (equal in value and type )
+// != ( not equal )
+// > ( greater than )
+// < ( less than ) 
+// >= ( greater than or equal to ) 
+// <= ( less than or equal to )
+
+
+// -- logical operator -- 
+
+// example of apersand && 
+
+const check = 4 > 3 && 10 > 5;      // true and true  -> true 
+const check2 = 4 > 3 && 10 < 5;     // true and false -> false 
+const check3 = 4 < 3 && 10 < 5;     // false && false -> false 
+
+
+// pipe or perator || 
+
+// ture || ture -> true 
+// true || false -> true 
+// false || false -> false 
+
+
+
+// Negation ! 
+
+let nega1 = 4 > 3;      // true 
+let nega2 = !(4 > 3);   // false 
+let nega3 = true        // true 
+let nega4 = !nega3      // false 
+let nega5 = !false      // true 
+
+
+
+// == increment operator == 
+
+// 1. Pre-increment 
+
+// let count = 0;
+// console.log(++count);
+// console.log(++count);
+// console.log(count);
+
+
+
+// 2. post-increment 
+
+// let count = 0;
+// console.log(count++);
+// console.log(count++);
+// console.log(count);
+
+
+
+// == Decrement operator == 
+
+// 1. pre-decrement 
+
+// let count = 0;
+// console.log(--count);
+// console.log(count);
+
+
+// 2. post-decrement 
+
+// let count = 0;
+// console.log(count--);
+// console.log(count);
+
+
+// == ternary operators == 
+
+// let isRaining = true;
+// isRaining
+//     ? console.log('You need a rain coat.')
+//     : console.log('No need for a rain coat')
+
+// isRaining = false; 
+// isRaining
+//     ? console.log('You need a rain coat.')
+//     : console.log('No need for a rain coat.')
+
+
+
+// let number = 0;
+// number > 0
+//     ? console.log(`${number} is a positive number`)
+//     : console.log(`${number} is a negative number`)
+
+
+
