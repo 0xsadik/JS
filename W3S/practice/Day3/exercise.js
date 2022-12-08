06286b3813bf382bd43b2aa4396ab4ea0491e60e
0xsadik <torikussadik404@gmail.com>
2022-12-08 23:13:55 +0600

    exercise

---
+++ W3S/practice/Day3/exercise.js
@@ -0,0 +1,32 @@
+
+// 1.
+
+
+// let firstName = 'Torikus';
+// let lastName = 'Sadik';
+// let country = 'Bangladesh';
+// let city = 'Dhaka';
+// let age = 24;
+// let isMarried = false;
+// let year = 2022;
+
+// console.log(typeof firstName);
+// console.log(typeof lastName);
+// console.log(typeof country);
+// console.log(typeof city);
+// console.log(typeof age);
+// console.log(typeof isMarried);
+// console.log(typeof year);
+
+
+
+// 2. 
+
+// console.log(typeof '10' == typeof 10);
+
+// 3. 
+
+// console.log(parseInt('9.8') == 10);
+
+// 4. 
+
