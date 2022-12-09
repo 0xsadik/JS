5aa0b2522364e39bfe92eaf7216cf066737a5252
0xsadik <torikussadik404@gmail.com>
2022-12-09 06:42:15 +0600

    Day 5

---
+++ W3S/practice/Day4/conditional.js
@@ -0,0 +1,72 @@
+
+// == if == 
+
+// let num = 3;
+// if (num > 0) {
+//     console.log(`${num} is a positive number`);
+// }
+
+
+// let isRaining = true;
+// if (isRaining) {
+//     console.log('Remember to take an umbrella');
+// }
+
+
+// == if else ==
+
+// let num = 3;
+// if ( num > 0) {
+//     console.log(`${num} is a positive number.`);
+// }
+// else {
+//     console.log(`${num} is a negative number.`);
+// }
+
+
+// let num = -3;
+// if (num > 0) {
+//     console.log(`${num} is a positive number.`);
+// }
+// else {
+//     console.log(`${num} is a negative number.`);
+// }
+
+
+// == if else if else ==
+
+// let a = 0;
+// if (a > 0) {
+//     console.log(`${a} is a positive number.`);
+// }
+// else if (a < 0) {
+//     console.log(`${a} is a negative number.`);
+// }
+// else if (a == 0) {
+//     console.log(`${a} is zero.`);
+// }
+// else {
+//     console.log(`${a} is not a number.`);
+// }
+
+
+
+//  == switch == 
+
+// let weather = 'cloudy';
+
+// switch (weather) {
+//     case 'rainy':
+//         console.log(`you need a rain coat.`);
+//         break; 
+//     case 'cloudy':
+//         console.log(`It might be cold, you need a jacket`);
+//         break;
+//     case 'sunny':
+//         console.log(`Go out freely`);
+//         break;
+//     default:
+//         console.log(`No need for rain coat`);
+// }
+
+
