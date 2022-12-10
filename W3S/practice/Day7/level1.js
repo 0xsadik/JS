d818758fa5bbc480782c7f5997b245b099f3f678
0xsadik <torikussadik404@gmail.com>
2022-12-10 21:44:18 +0600

    level - 1

---
+++ W3S/practice/Day7/level1.js
@@ -0,0 +1,100 @@
+// exercise [ level - 1 ]
+
+// 1. Declare a function fullName and it print out your full name 
+
+// function fullName() {
+//     console.log('Torikus Sadik');
+// }
+// fullName();
+
+
+// 2. Declare a function fullName and now it takes firstName, lastName as a parameter and it returns your full - name.
+
+// function fullName(firstName, lastName) {
+//     return firstName + ' ' + lastName;
+// }
+// console.log(fullName('Torikus', 'Sadik'));
+
+
+// 3. Declare a function addNumbers and it takes two two parameters and it returns sum.
+
+// function addNumbers(num1, num2) {
+//     return num1 + num2;
+// }
+// console.log(addNumbers(10, 30));
+
+
+// 4. An area of a rectangle is calculated as follows: area = length x width. Write a function which calculates areaOfRectangle.
+
+// function areaOfRectangle(length, width) {
+//     let area = length * width;
+//     return area;
+// }
+// console.log(areaOfRectangle(12, 7));
+
+
+// 5. A perimeter of a rectangle is calculated as follows: perimeter= 2x(length + width). Write a function which calculates perimeterOfRectangle.
+
+// function perimeterOfRectangle(length, width) {
+//     let perimeter = 2 * (length + width);
+//     return perimeter;
+// }
+// console.log(perimeterOfRectangle(12, 7));
+
+
+// 6. A volume of a rectangular prism is calculated as follows: volume = length x width x height. Write a function which calculates volumeOfRectPrism.
+
+// function volumeOfRectPrism(length, width, height) {
+//     let volume = length * width * height;
+//     console.log(volume);
+// }
+// volumeOfRectPrism(7, 12, 15);
+
+
+// 7. Area of a circle is calculated as follows: area = π x r x r. Write a function which calculates areaOfCircle
+
+// function areaOfCircle(r) {
+//     let pi = 3.14;
+//     let area = pi * r * r;
+//     return area;
+// }
+// console.log(areaOfCircle(15));
+
+
+// 8. Circumference of a circle is calculated as follows: circumference = 2πr. Write a function which calculates circumOfCircle
+
+// function circumOfCircle(r) {
+//     let pi = 3.14;
+//     let circumference = 2 * pi * r;
+//     return circumference;
+// }
+// console.log(circumOfCircle(15));
+
+
+// 9. Density of a substance is calculated as follows:density= mass/volume. Write a function which calculates density.
+
+// function density(mass, volume) {
+//     let dens = mass / volume;
+//     return dens;
+// }
+// console.log(density(8900, 2));
+
+
+// 10. Speed is calculated by dividing the total distance covered by a moving object divided by the total amount of time taken. Write a function which calculates a speed of a moving object, speed.
+
+// function speedCal(distance, time) {
+//     let speed = distance / time;
+//     return speed;
+// }
+// console.log(speedCal(99,  5));
+
+
+// 11. Weight of a substance is calculated as follows: weight = mass x gravity. Write a function which calculates weight.
+
+
+// function calculateWeight (mass, gravity) {
+//     let weight  = mass * gravity;
+//     return weight;
+// }
+// console.log(calculateWeight(120, 9.8));
+
