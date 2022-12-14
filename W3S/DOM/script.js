36cb5851b57e72c462614970064278cc13bcb709
0xsadik <torikussadik404@gmail.com>
2022-12-14 18:34:24 +0600

    dom

---
+++ W3S/DOM/script.js
@@ -0,0 +1,23 @@
+
+
+// 1. --------------
+
+// -> get element by id 
+
+// const title = document.getElementById('main-heading');
+// console.log(title);
+
+// -> get element by className 
+
+// const listItem = document.getElementsByClassName('list-items');
+// console.log(listItem);
+
+// -> get element by tagName 
+
+// const listItem = document.getElementsByTagName('li');
+// console.log(listItem);
+
+// -> get element by querySelector 
+
+const container = document.querySelector('div');
+console.log(container);
