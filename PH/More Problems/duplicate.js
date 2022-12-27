c3ad6da4d2e96148fc128d7dd785f62c2f01f18f
0xsadik <torikussadik404@gmail.com>
2022-12-27 11:53:32 -0800

    ph

---
+++ PH/More Problems/duplicate.js
@@ -0,0 +1,15 @@
+const names = ['abul', 'babul', 'cabul', 'dabul', 'ebul', 'fabul', 'babul', 'ebul', 'fabul', 'babul', 'ebul', 'babul', 'gabul', 'abul', 'habul', 'dabul'];
+
+function removeDuplicate(names) {
+    const unique = [];
+    // for (let i = 0; i < names.length; i++) {
+    //     const element = names[i];
+    //     // console.log(element);
+    // }
+
+    for (element of names) {
+        console.log(elements);
+        if (unique.indexOf(elements))
+    }
+}
+removeDuplicate(names)
