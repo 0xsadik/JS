134584124bdb41ac1d5b2aaace9199e3be115aa5
0xsadik <torikussadik404@gmail.com>
2023-01-05 14:52:18 -0800

    prac

--- Practice/Array/9.basics.js
+++ Practice/Array/9.basics.js
@@ -6,9 +6,34 @@ const arr = [
     [7, 8, 9],
     [[10, 11, 12], 13, 14]
 ];
-const subArr = arr[3];
-const nestedSubArr = arr[3][0];
-const element = arr[3][0][1];
-console.log(subArr);
-console.log(nestedSubArr);
-console.log(element);
+// const subArr = arr[3];
+// const nestedSubArr = arr[3][0];
+// const element = arr[3][0][1];
+// console.log(subArr);
+// console.log(nestedSubArr);
+// console.log(element);
+
+// const find = arr[2][1];
+// console.log(find);
+
+
+// using push 
+
+// const arr1 = [1, 2, 3];
+// arr1.push(4);
+// console.log(arr1);
+
+// const arr2 = ['torikus', 'sadik', 'tori'];
+// arr2.push(['happy', 'depression']);
+// console.log(arr2);
+
+
+// using pop 
+
+// const threeArr = [1, 2, 3];
+// const onedel = threeArr.pop();
+// console.log(threeArr);
+
+const myArray = [["John", 23], ["cat", 2]];
+myArray.pop();
+console.log(myArray);
